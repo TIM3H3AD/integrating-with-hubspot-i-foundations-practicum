4528ab370ac5b6b30a297c806a45ff243014bd7c
wb <wb@313.CASH>
2026-05-26 15:49:05 -0400

    Add Extra homepage table and 8-bit styling

--- index.js
+++ index.js
@@ -15,7 +15,24 @@ const HUBSPOT_OBJECTS_URL = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJE
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const resp = await axios.get(`${HUBSPOT_OBJECTS_URL}?properties=extra_offerings,thing_id,creator,ownership_id`, { headers });
+        const data = resp.data.results;
+        res.render('homepage', {
+            title: 'Extras | Integrating With HubSpot I Practicum',
+            data
+        });
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to retrieve Extra records from HubSpot.');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
