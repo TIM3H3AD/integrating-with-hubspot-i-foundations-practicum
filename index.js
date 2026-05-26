9afc14930a66b530868aa5cee3e2fbb319da9790
wb <wb@313.CASH>
2026-05-26 15:30:38 -0400

    Add Extra custom object create route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,7 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const CUSTOM_OBJECT_TYPE = 'p51492116_extras';
+const HUBSPOT_OBJECTS_URL = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -24,7 +27,29 @@ app.get('/update-cobj', (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const extraRecord = {
+        properties: {
+            extra_offerings: req.body.name,
+            thing_id: req.body.thing_id,
+            creator: req.body.user_id,
+            ownership_id: req.body.ownership_id
+        }
+    };
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(HUBSPOT_OBJECTS_URL, extraRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to create Extra record in HubSpot.');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
